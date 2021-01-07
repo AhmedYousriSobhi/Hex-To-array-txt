a5990edf5f97b38be278e3ecc855835923044214
Ahmed Yousri <66730765+AhmedYousriSobhi@users.noreply.github.com>
2021-01-07 19:05:37 +0200

    Add files via upload

--- hextoarray.c
+++ hextoarray.c
@@ -1,52 +1,54 @@
 #include<stdio.h>
-#define CODESIZE 128
+#include<stdint.h>
+#define PAGESIZE 128
+#define FILLBYTES 16
 int main(int argc, char *argv[])
 {
-	char tempc, datasize, recorddatasize, recordindex = 0, loopindex=0, flagfirsttime=1, eof=0, stored=0;
-	int bytecount1, bytecount2, numofpage=1;
-	int arraybytecounter = 0;
+	uint8_t tempc, datasize, recordDataSize, loopindex=0, flagfirsttime=1, eof=0, stored=0;
+	uint16_t byteCount_1, byteCount_2, NoOfPage=1;
+	uint16_t arrayByteCounter = 0;
 	FILE *cfptr_hex;
 	FILE *cfptr_txt;
-	printf("*Program STARTED*\n");
+	printf("*C_DEBUG: Program STARTED*\n");
 	if((cfptr_hex = fopen(argv[1],"r")) == NULL)
 	{
-		printf("*File could not be openned*\n");
+		printf("*C_DEBUG: File could not be openned*\n");
 	}
 	else 
 	{
-		printf("*Hex File is Openned!!*\n");
+		printf("*C_DEBUG: Hex File is Openned!!*\n");
 		if((cfptr_txt = fopen("OutputArray.txt","w")) == NULL)
 			{
-				printf("*Could not Create Txt file*\n");
+				printf("*C_DEBUG: Could not Create Txt file*\n");
+				fclose(cfptr_hex);
+				printf("C_DEBUG: HEX FILE IS CLOSED SUCCESSFULY!!!\n");
 			}
 		else
 		{	
-			printf("*OutputArray.txt is Created*\n");
-			printf("*Starting Read Bytes from Hex File*\n");	
-			printf("*Checking Hex File*\n");
+			printf("*C_DEBUG: OutputArray.txt IS CREATED*\n");
+			printf("*C_DEBUG: STARTING READ BYTES FROM Hex FILE*\n");	
+			printf("*C_DEBUG: CHECKING HEX FILE*\n");
 			fprintf(cfptr_txt,"%s", "{"); /*Starting of the output array*/
 			fputc('\n',cfptr_txt); /*new line*/
-			/*read bytes from Hex file*/
+			/*read bytes of each signle Line from Hex file*/
 			while( (fgetc(cfptr_hex) == ':') && (eof == 0) ) 
 			{
-				recordindex++;
 				/*
-				printf("*Hex record is Correct number = ");
+				printf("*C_DEBUG: Hex record is Correct number = ");
 				printf("%d *\n", recordindex);
 				*/
-				/*Check Byte Count in Intel Hex Format:
-				  assumed only 32 or 16 data byte size is used.*/
-				bytecount1 = (fgetc(cfptr_hex) - '0') * 16;
-				bytecount2 = fgetc(cfptr_hex) - '0';
-				recorddatasize = bytecount1 + bytecount2;
-				if(stored == 0) {datasize = recorddatasize;stored =1;} 
-				/*printf("DataSize of record = %d\n", recorddatasize);*/
-				if(recorddatasize != 0)
+				/*Check Byte Count in Intel Hex Format:*/
+				byteCount_1 = (fgetc(cfptr_hex) - '0')*16;
+				byteCount_2 = fgetc(cfptr_hex) - '0';
+				recordDataSize = byteCount_1 + byteCount_2;
+				arrayByteCounter += recordDataSize;	
+				printf("C_DEBUG: DataSize in line = %d\n", recordDataSize);
+				if(recordDataSize != 0)
 				{	
 					/*Igonre the coming 6 address and record type bytes*/
 					for(loopindex =0; loopindex<6; loopindex++) tempc=fgetc(cfptr_hex);
 					/*Reading the data bytes*/
-					for(loopindex=0; loopindex<recorddatasize; loopindex++)
+					for(loopindex=0; loopindex<recordDataSize; loopindex++)
 					{
 						fprintf(cfptr_txt,"%s", " 0x");
 						fputc(fgetc(cfptr_hex),cfptr_txt);
@@ -62,26 +64,29 @@ int main(int argc, char *argv[])
 				}
 				else eof = 1;	
 			}
+			printf("HERE");
+			while(arrayByteCounter > PAGESIZE*NoOfPage)
+			{	
+				NoOfPage++;
+			}
+			uint16_t NoFillBytes = PAGESIZE*NoOfPage - arrayByteCounter;
+			/*Fill Bytes are placed 16 bytes line length*/			
+			for(loopindex = 1; loopindex<=(NoFillBytes); loopindex++)
+				{					
+					if(NoFillBytes - loopindex == 0) fprintf(cfptr_txt,"%s", " 0xFF");
+					else fprintf(cfptr_txt,"%s", " 0xFF,");
+					//printf("%d\n", loopindex%FILLBYTES);
+					if(loopindex%FILLBYTES == 0) fputc('\n',cfptr_txt); /*new line*/
+				}				
+			fprintf(cfptr_txt,"%s","\n};\n\n");
+			fprintf(cfptr_txt,"%s","#define CODE_SIZE ");
+			fprintf(cfptr_txt,"%d\n",PAGESIZE*NoOfPage);
+			fprintf(cfptr_txt,"%s","#define NO_OF_PAGES ");
+			fprintf(cfptr_txt,"%d\n",NoOfPage);
+			fclose(cfptr_txt);	
+			fclose(cfptr_hex);
+			printf("C_DEBUG: .TXT FILE IS CREATED SUCCESSFULY!!!\n");
 		}
-		while(datasize*(recordindex-1) > CODESIZE*numofpage)
-		{	
-			numofpage++;
-		}	
-		for(loopindex = 1; loopindex<=(CODESIZE*numofpage - datasize*(recordindex-1)); loopindex++)
-			{
-				if(CODESIZE*numofpage - datasize*(recordindex-1) - loopindex == 0) fprintf(cfptr_txt,"%s", " 0xFF");
-				else fprintf(cfptr_txt,"%s", " 0xFF,");
-				//printf("%d\n", loopindex%datasize);
-			    if(loopindex%datasize == 0) fputc('\n',cfptr_txt); /*new line*/
-			}				
-		fprintf(cfptr_txt,"%s","\n};\n\n");
-		fprintf(cfptr_txt,"%s","#define CODE_SIZE ");
-		fprintf(cfptr_txt,"%d\n",CODESIZE*numofpage);
-		fprintf(cfptr_txt,"%s","#define NUM_OF_PAGES ");
-		fprintf(cfptr_txt,"%d\n",numofpage);
-		fclose(cfptr_txt);
 	}
-	fclose(cfptr_hex);
-	printf("txt File Created SUCCESSFULY!!!\n");
 	return 0;
 }
